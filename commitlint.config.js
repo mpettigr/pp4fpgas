fe1656e4ed74469741b1af872cce7d41fd24b8bb
Mark Pettigrew <mark.pettigrew@amd.com>
2024-06-12 15:18:20 -0700

    fix: use more generic rule with broader application

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,10 @@
 module.exports = {
-    rules: {
-      'body-max-line-length': [2, 'always', 100], // Default rule
-    },
-    plugins: [
-      {
-        rules: {
-          'ignore-body-max-line-length-for-release': ({type, body}) => {
-            if (type === 'chore' && body.includes('release')) {
-              return [0]; // Disable the rule for release commits
-            }
-            return [2, 'always', 100]; // Default rule for other commits
-          }
-        }
+  rules: {
+    'body-max-line-length': ({body}) => {
+      if (body && body.startsWith('chore(release):')) {
+        return [0]; // Disable the rule for release commits
       }
-    ],
-    extends: [
-      '@commitlint/config-conventional'
-    ],
-    rules: {
-      'ignore-body-max-line-length-for-release': [2, 'always']
-    },
-  };
+      return [2, 'always', 100]; // Default rule for other commits
+    }
+  },
+};
